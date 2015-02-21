a0a629cb3f0a81e24522cbde6f0328eec70f7c14
B F <benfiola@gmail.com>
2015-02-20 21:57:13 -0800

    Basics are working - zoom is off - don't want to hardcode field of view or zoom.

--- src/com/ben/javaengine/graphics/converter/ThreeDimensionalConverter.java
+++ src/com/ben/javaengine/graphics/converter/ThreeDimensionalConverter.java
@@ -44,26 +44,19 @@ public class ThreeDimensionalConverter extends RelativeTopDownConverter {
 				Vertex bl = w.getBottomLeft();
 				Vertex tr = w.getTopRight();
 				Vertex br = w.getBottomRight();
+				Point pTl = transformVertex(tl, p, xFactor, yFactor);
+				Point pTr = transformVertex(tr, p, xFactor, yFactor);
+				Point pBl = transformVertex(bl, p, xFactor, yFactor);
+				Point pBr = transformVertex(br, p, xFactor, yFactor);
 				
-				Point p1 = transformVertex(tl, p, xFactor, yFactor);
-				Point p2 = transformVertex(tr, p, xFactor, yFactor);
-				lines.add(new LineGraphicData(p1.x, p1.y, p2.x,
-						p2.y, Color.YELLOW));
-				
-				p1 = transformVertex(tr, p, xFactor, yFactor);
-				p2 = transformVertex(br, p, xFactor, yFactor);
-				lines.add(new LineGraphicData(p1.x, p1.y, p2.x,
-						p2.y, Color.YELLOW));
-				
-				p1 = transformVertex(bl, p, xFactor, yFactor);
-				p2 = transformVertex(br, p, xFactor, yFactor);
-				lines.add(new LineGraphicData(p1.x, p1.y, p2.x,
-						p2.y, Color.YELLOW));
-				
-				p1 = transformVertex(br, p, xFactor, yFactor);
-				p2 = transformVertex(tr, p, xFactor, yFactor);
-				lines.add(new LineGraphicData(p1.x, p1.y, p2.x,
-						p2.y, Color.YELLOW));
+				lines.add(new LineGraphicData(pTl.x, pTl.y, pTr.x,
+						pTr.y, Color.YELLOW));
+				lines.add(new LineGraphicData(pTr.x, pTr.y, pBr.x,
+						pBr.y, Color.YELLOW));
+				lines.add(new LineGraphicData(pBr.x, pBr.y, pBl.x,
+						pBl.y, Color.YELLOW));
+				lines.add(new LineGraphicData(pBl.x, pBl.y, pTl.x,
+						pTl.y, Color.YELLOW));
 				toReturn.add(new WallGraphicData(lines, Color.RED));
 			}
 		}
@@ -73,9 +66,16 @@ public class ThreeDimensionalConverter extends RelativeTopDownConverter {
 	private Point transformVertex(Vertex toTransform, Player p, Double xFactor, Double yFactor){
 		Vertex dV = translateAndRotate(toTransform, p);
 		Vertex eV = new Vertex( (double) (panel.getWidth()/2), (double) (panel.getHeight()/2), 0.0);
-		Integer pBx = Rounder.round(eV.getX() + (eV.getX() * dV.getX())/dV.getZ());
-		Integer pBy = Rounder.round(eV.getY() + (eV.getY() * dV.getY())/dV.getZ());
-		return new Point(pBx, pBy);
+	
+		Double zoomFactor = 10.0;
+		Double ratio = 45.0;
+		/*
+		Integer pBx = Rounder.round(eV.getX() + (eV.getY() * dV.getX())/dV.getY());
+		Integer pBy = Rounder.round(eV.getY() + (eV.getY() * dV.getZ())/dV.getY()); */
+		Integer pBx = Rounder.round( eV.getX() + ((zoomFactor * ratio) * (dV.getY()/dV.getX()) ) );
+		Integer pBy = Rounder.round( eV.getY() + (zoomFactor * (dV.getZ()/dV.getX()) ) );
+		Point toReturn = new Point(pBx, pBy);
+		return toReturn;
 	}
 
 }
